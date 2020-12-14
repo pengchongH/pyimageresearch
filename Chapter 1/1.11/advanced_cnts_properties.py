992428d212f1e92b83c7aeda0dc96838fc276c48
pleasey0u <35575825+pleasey0u@users.noreply.github.com>
2020-12-14 19:46:07 +0800

    Add files via upload

---
+++ Chapter 1/1.11/advanced_cnts_properties.py
@@ -0,0 +1,21 @@
+# Advanced contours properties
+# always consider contours before more advanced computer vision and machine learning methods
+
+
+# Aspect Ratio
+# aspect ratio = image width / image height
+
+# Extent
+# extent = shape area(contour area) / bounding box area
+# USE: distinguish between various shapes in images
+
+# Convex Hull
+# A convex hull is the smallest possible convex set that contains X points which belong to a set in Euclidean space.
+# like a mathematical rubber band
+# convexity defect: If a curve is not bulged out
+# USE: 1. hand gesture recognition
+#      2. compute solidity
+
+# Solidity
+# solidity = contour area / convex hull area
+# USE: distinguish between various objects
